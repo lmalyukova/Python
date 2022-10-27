44466f6d355967705d9bf9a36f837812a974e948
lenaMalyukova <115982740+lmalyukova@users.noreply.github.com>
2022-10-27 10:17:03 +0300

    Add files via upload

---
+++ pr_3.py
@@ -0,0 +1,9 @@
+a = int(input('Первое число: \n'))
+b = int(input('Второе число: \n'))
+c = int(input('Третье число: \n'))
+if (a >= 1) and (a <= 3):
+    print(a)
+if (b >= 1) and (b <= 3):
+    print(b)
+if (c >= 1) and (c <= 3):
+    print(c)
